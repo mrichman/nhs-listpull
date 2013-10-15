4ecaafd0fd8d124e9e43a1df49ddd5922da961dc
Mark Richman <mark@markrichman.com>
2013-10-15 15:16:33 -0400

    Updated to work with gunicorn

--- run.py
+++ run.py
@@ -4,4 +4,8 @@
 from listpull import app
 
 
-app.run(threaded=True, host='0.0.0.0', port=80, debug=True)
+if __name__ == '__main__':
+    app.run()
+
+# app.run(threaded=True, host='0.0.0.0', port=80, debug=True)
+# sudo gunicorn -D -b 10.139.16.14:80 run:app
