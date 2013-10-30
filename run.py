9858658bb3e10f5617f6540eaf646d6c05452120
Mark Richman <mark@markrichman.com>
2013-10-30 12:35:49 -0400

    Updated gunicorn to use conf file

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     app.run()
 
 # app.run(threaded=True, host='0.0.0.0', port=80, debug=True)
-# sudo gunicorn -D -b 10.139.16.14:80 run:app
+# sudo gunicorn -c gunicorn.conf.py run:app
